aeea40be329dd80b818129e40fec36ee0a905c5b
Franck <duzant.franck@gmail.com>
2023-10-28 19:28:04 +0200

    ex01 fix

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,31 +6,11 @@
 /*   By: fduzant <fduzant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:18:10 by fduzant           #+#    #+#             */
-/*   Updated: 2023/10/28 16:07:32 by fduzant          ###   ########.fr       */
+/*   Updated: 2023/10/28 18:58:51 by fduzant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-
-float	ft_power(float base, int exponant)
-{
-	float	result;
-
-	if (!exponant)
-		return (1);
-	if (exponant < 0)
-	{
-		base = 1 / base;
-		exponant *= -1;
-	}
-	result = base;
-	while(exponant > 0)
-	{
-		result *= base;
-		exponant--;
-	}
-	return (result);
-}
 
 Fixed::Fixed() : _value(0)
 {
@@ -43,12 +23,12 @@ Fixed::Fixed(const Fixed &fixed)
 	*this = fixed;
 }
 
-Fixed::Fixed(const int value) : _value(value * ft_power(2, _fractional_bits))
+Fixed::Fixed(const int value) : _value(value << _fractional_bits)
 {
 	std::cout << "Int constructor called" << std::endl;
 }
 
-Fixed::Fixed(const float value) : _value(value * ft_power(2, _fractional_bits))
+Fixed::Fixed(const float value) : _value(roundf(value * (1 << _fractional_bits)))
 {
 	std::cout << "Float constructor called" << std::endl;
 }
@@ -78,14 +58,14 @@ int		Fixed::getRawBits(void) const
 
 int		Fixed::toInt(void) const
 {
-	return (this->_value * ft_power(2, -_fractional_bits));
+	return (this->_value >> _fractional_bits);
 }
 
 float	Fixed::toFloat(void) const
 {
-	return (this->_value * ft_power(2, -_fractional_bits));
+	return ((float)this->_value / (float)(1 << _fractional_bits));
 }
-//with roundf function from cmath
+
 std::ostream	&operator<<(std::ostream &str, Fixed const &fixed_nbr)
 {
 	return (str << fixed_nbr.toFloat());
