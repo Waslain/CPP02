d83c2e95dfb34fcac45f55b2adb1e69ff107e6ef
Franck <duzant.franck@gmail.com>
2024-01-31 12:28:47 +0100

    CPP02 should be good

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,13 +6,13 @@
 /*   By: fduzant <fduzant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:18:10 by fduzant           #+#    #+#             */
-/*   Updated: 2023/10/27 16:31:21 by fduzant          ###   ########.fr       */
+/*   Updated: 2024/01/31 12:19:31 by fduzant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed() : value(0)
+Fixed::Fixed() : _value(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
@@ -31,17 +31,17 @@ Fixed::Fixed(const Fixed &fixed)
 Fixed	&Fixed::operator=(const Fixed &fixed)
 {
 	std::cout << "Assignation operator called" << std::endl;
-	this->value = fixed.getRawBits();
+	this->_value = fixed.getRawBits();
 	return (*this);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	this->value = raw;
+	this->_value = raw;
 }
 
 int		Fixed::getRawBits(void) const
 {
 	std::cout << "getRawBits member function called" << std::endl;
-	return (this->value);
+	return (this->_value);
 }
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: fduzant <fduzant@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 14:36:03 by fduzant           #+#    #+#             */
-/*   Updated: 2023/10/27 16:26:38 by fduzant          ###   ########.fr       */
+/*   Updated: 2024/01/31 12:15:27 by fduzant          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ class	Fixed
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 	private:
-		int					value;
-		static const int	fractional_bits = 8;
+		int					_value;
+		static const int	_fractional_bits = 8;
 };
 
 #endif
